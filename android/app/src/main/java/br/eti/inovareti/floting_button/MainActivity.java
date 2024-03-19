9443d79cb502567c13984700d5f0a67201d19efc
Iraponan Marinho <iraponan.aires@gmail.com>
2024-03-18 21:53:06 -0300

    Configurando o app.

--- android/app/src/main/java/br/eti/inovareti/floting_button/MainActivity.java
+++ android/app/src/main/java/br/eti/inovareti/floting_button/MainActivity.java
@@ -1,6 +1,25 @@
 package br.eti.inovareti.floting_button;
 
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import com.yhao.floatwindow.FloatWindow;
+import com.yhao.floatwindow.Screen;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "floating_button";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler((call, result) -> {
+
+        });
+    }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const platform = MethodChannel('floating_button');
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+}
